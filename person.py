2a928aafb549091bb2151eb931f3c52586652593
dagazrev <33989743+dagazrev@users.noreply.github.com>
2022-12-30 11:17:42 +0100

    Comments for documentation
    Necessary comment to generate a "quick-and-dirty" documentation using Pycco.

--- person.py
+++ person.py
@@ -2,9 +2,20 @@ import pygame
 import os
 import random
 
-# class that models a single person
+# === Class that models a single person. ===
+
 class Person:
+#Attributes:
+#weight (int) not float for sake of simplicity
+#gender (bool)
+#age (int) age of the person as an int
+#enter_floor
+#exit_floor    
+    
     def __init__(self, enter_floor, exit_floor, weight=60, gender=True, age=32):
+        """
+        Initializes all the parameters and assign in GUI the proper image asset according to gender
+        """
         self.weight = weight
         self.gender = gender
         self.age = age
@@ -20,6 +31,7 @@ class Person:
         self.img = pygame.transform.smoothscale(self.img,(20, 60))
 
     def __str__(self):
+        #Override of STR to properly display in Console info about the enter and exit floor of each agent.
         return "[Enter: %d, Exit: %d]"%(self.enter_floor, self.exit_floor)
 
 
